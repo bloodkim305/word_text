c9f59fe98f034be23a85abee67794fcfaed23db3
bloodkim305 <46419265+bloodkim305@users.noreply.github.com>
2020-07-23 16:16:58 +0700

    Updates

---
+++ precache-manifest.d47c1e2e855d15521528c429d93e9578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258ba00a605649f72fbf7ea6142512e4",
+    "url": "/word_text/index.html"
+  },
+  {
+    "revision": "909e9cb9beb29b92a897",
+    "url": "/word_text/static/css/main.c3e7c25d.chunk.css"
+  },
+  {
+    "revision": "8c3ebee9d8e953f16cd2",
+    "url": "/word_text/static/js/2.5b98d68e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/word_text/static/js/2.5b98d68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909e9cb9beb29b92a897",
+    "url": "/word_text/static/js/main.28999a1e.chunk.js"
+  },
+  {
+    "revision": "f03e0c16ebd9ac0d5dd6",
+    "url": "/word_text/static/js/runtime-main.5ecf7c3a.js"
+  }
+]);
